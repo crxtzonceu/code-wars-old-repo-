6599734af6482aed9a453b2a502c45183c222b36
crxtzonceu <zamanthaquijano@gmail.com>
2022-04-24 00:17:06 +0800

    added

--- 8 kyu/counting sheep/ans.js
+++ 8 kyu/counting sheep/ans.js
@@ -1,1 +1,11 @@
 const countSheeps = n => n.map(x => Boolean(x)).reduce((a, b) => a + b)
+
+
+
+//  much better answer, it uses truthy value
+
+const countSheeps = n => n.map(Boolean).length
+
+// or
+
+const countSheeps = n => n.map(x => x).length
